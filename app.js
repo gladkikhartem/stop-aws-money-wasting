c32826cf6920ed7ccdd7025cc5b396d5013342e0
Artem Gladkikh <artem.gladkikh@idt.net>
2020-11-23 15:13:36 +0300

    save

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-require(['aws-sdk'], function (AWS, CONF) {    
-    function init(){
-        AWS.config = new AWS.Config({
-            access_key_id: "CONF.AWS_ACCESS_KEY_ID",
-            secretAccessKey: "CONF.AWS_SECRET_ACCESS_KEY",
-            region: "us-east-1"
-        });
-        var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+require(['aws-sdk'], function (AWS) {
+    console.log("HELLdddddOW")
+    // function init() {
+    //     console.log("HELLddddOW")
+    //       AWS.config = new AWS.Config({
+    //         access_key_id: document.getElementById("AWS_ACCESS_KEY_ID").value,
+    //         secretAccessKey: document.getElementById("AWS_SECRET_ACCESS_KEY").value,
+    //         region: "us-east-1"
+    //     });
+        
+    //     var ec2 = new AWS.EC2();
+    //     var res = ec2.describeVolumes().promise();
+    //     console.log(res)
+    // }
 
-    }
-});          
+});    
+
+var createReport = function() {
+    console.log("HELLdddddOW")
+          
+}
